f7db97ceece70bebd71ab33bcf88a052f81ca767
Alyera Monzon <amonzon@fospuca.com>
2025-01-16 15:40:13 -0400

    a

--- src/client-information/client-information.controller.ts
+++ src/client-information/client-information.controller.ts
@@ -7,7 +7,7 @@ import { UpdateClientInformationDto } from './dto/update-client-information.dto'
 export class ClientInformationController {
   constructor(private readonly clientInformationService: ClientInformationService) {}
 
-  @Get(':PAGE/:COMPANY')
+  @Get('info/:PAGE/:COMPANY')
     async getClientInfo(
       @Param('PAGE') PAGE: string,
       @Param('COMPANY') COMPANY: string,
@@ -16,25 +16,25 @@ export class ClientInformationController {
         const pageNumber = parseInt(PAGE, 10);
       
         if(COMPANY==='F5618'){
-          return this.clientInformationService.getClientInformationByRifChacao(pageNumber);
+          return this.clientInformationService.getClientInformationChacao(pageNumber);
         }else if(COMPANY==='F0016'){
-          return this.clientInformationService.getClientInformationByRifManeiro(pageNumber);
+          return this.clientInformationService.getClientInformationManeiro(pageNumber);
         }else if(COMPANY==='F0015'){
-          return this.clientInformationService.getClientInformationByRifCaroni(pageNumber);
+          return this.clientInformationService.getClientInformationCaroni(pageNumber);
         }else if(COMPANY==='F0004'){
-          return this.clientInformationService.getClientInformationByRifHatillo(pageNumber);
+          return this.clientInformationService.getClientInformationHatillo(pageNumber);
         }else if(COMPANY==='F1099'){
-          return this.clientInformationService.getClientInformationByRifBaruta(pageNumber);
+          return this.clientInformationService.getClientInformationBaruta(pageNumber);
         }else if(COMPANY==='S6759'){
-          return this.clientInformationService.getClientInformationByRifSanDiego(pageNumber);
+          return this.clientInformationService.getClientInformationSanDiego(pageNumber);
         }else if(COMPANY==='F7834'){
-          return this.clientInformationService.getClientInformationByRifElTigre(pageNumber);
+          return this.clientInformationService.getClientInformationElTigre(pageNumber);
         }else if(COMPANY==='F2099'){
-          return this.clientInformationService.getClientInformationByRifInvBaruta(pageNumber);
+          return this.clientInformationService.getClientInformationInvBaruta(pageNumber);
         }
     }
 
-    @Get(':CUSTNMBR/:COMPANY')
+    @Get('rif/:CUSTNMBR/:COMPANY')
     async getClientInfobyRif(
       @Param('CUSTNMBR') CUSTNMBR: string,
       @Param('COMPANY') COMPANY: string,
