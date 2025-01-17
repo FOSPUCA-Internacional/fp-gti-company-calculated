fa5a81edf5dfc679bc43886584b76fb0a1452eb9
Alyera Monzon <amonzon@fospuca.com>
2025-01-17 17:06:01 -0400

    .

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['*'], 
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], 
+    origin: 'http://localhost:3001', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true, 
   });
   await app.listen(4007);
